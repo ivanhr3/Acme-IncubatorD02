704bda3ee51cdec0479ae6058b64cf0997203e5b
ivanhr3 <ivaherrod1@alum.us.es>
2020-08-13 21:28:52 +0200

    Entities and features

---
+++ src/main/java/acme/entities/notices/Notice.java
@@ -0,0 +1,51 @@
+
+package acme.entities.notices;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends DomainEntity {
+
+	/**
+	 *
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@URL
+	private String				headerPicture;
+
+	@NotBlank
+	private String				title;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				creationDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				deadline;
+
+	@NotBlank
+	private String				body;
+
+	private Collection<String>	relatedNotices;
+
+}
